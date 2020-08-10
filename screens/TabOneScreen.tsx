527f25c3fd5c9072a451f3cf3dc1647387ebac64
nisten <nisten@outlook.com>
2020-08-10 18:27:45 -0400

    bootstrap 5 tryout

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
+import { Button } from 'bootstrap';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      
       <Text style={styles.title}>Full Stack Cat Tree</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Button>Mmoo</Button>
     </View>
+    
   );
 }
 
